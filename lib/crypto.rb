c64f75a332388b359208cd04c7634d48de550975
MuTtonye91 <murielle.akoo@live.fr>
2019-01-17 11:23:28 +0100

    Add code in crypto prog

--- lib/crypto.rb
+++ lib/crypto.rb
@@ -1,2 +1,8 @@
+require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
+
+page  = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/")) 
+puts page.css("td[class=text-left col-symbol]").text
+
+
