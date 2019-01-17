ed1b267abfca839a46e319859f0a25cc128e88f3
MuTtonye91 <murielle.akoo@live.fr>
2019-01-17 14:31:30 +0100

    Add crypto prog

--- lib/crypto.rb
+++ lib/crypto.rb
@@ -3,6 +3,22 @@ require 'nokogiri'
 require 'open-uri'
 
 page  = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/")) 
-puts page.css("td[class=text-left col-symbol]").text
+# get elements of the site
+		
+symbol = page.xpath('//*[@class="text-left col-symbol"]')
+price = page.xpath('//*[@class="price"]')
 
 
+ #creat empty array
+symbol_array = []
+price_array = []
+
+# insert elements in array
+symbol.each {|items| symbol_array << items.text}
+price.each {|elemt| price_array << elemt.text[1..-1].to_f}
+
+a = []
+symbol_array.each_with_index do |items, index|
+	a << {items => price_array[index]}
+end
+puts a
--- lib/mairie.rb
+++ lib/mairie.rb
@@ -1,2 +1,17 @@
+require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
+
+
+page  = Nokogiri::HTML(open("http://annuaire-des-mairies.com/val-d-oise.html")) 
+
+# get elements of the site
+cities = page.xpath('//*[@class="lientxt"]').text
+
+
+cities_array = []
+cities_array.each {|items| cities_array << items}
+
+puts cities
+
+
