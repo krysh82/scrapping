5ae5e0b726c45a084d4b4798f4bf6b709fafa77d
MuTtonye91 <murielle.akoo@live.fr>
2019-01-17 17:35:29 +0100

    Add mairie prog

--- lib/mairie.rb
+++ lib/mairie.rb
@@ -2,16 +2,43 @@ require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
 
+def townhall_page
+	page = Nokogiri::HTML(open("https://www.annuaire-des-mairies.com/val-d-oise.html"))
+	return page
+end
 
-page  = Nokogiri::HTML(open("http://annuaire-des-mairies.com/val-d-oise.html")) 
 
-# get elements of the site
-cities = page.xpath('//*[@class="lientxt"]').text
+def get_townhall_email(townhall_url)
+	town_array = []
+	page = Nokogiri::HTML(open(townhall_url))
+
+	email = page.xpath('/html/body/div/main/section[2]/div/table/tbody/tr[4]/td[2]').text
+	town = page.xpath('/html/body/div/main/section[1]/div/div/div/h1').text
+
+	town_array << {town => email}
+	puts town_array
+end
 
 
-cities_array = []
-cities_array.each {|items| cities_array << items}
+def get_townhall_url
+	url_array = []
+	page = townhall_page
 
-puts cities
+	urls = page.xpath('//*[@class="lientxt"]/@href')
 
+	urls.each do |url|
+		url = "http://annuaire-des-mairies.com" + url.text[1..-1]
+		url_array << url		
+	end
+	return url_array 
+end
+
+def perform
+	url_array = get_townhall_url
+	url_array.each do |townhall_url|
+		get_townhall_email(townhall_url)
+	end
+end
+
+perform
 
